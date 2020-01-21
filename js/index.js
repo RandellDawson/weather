539ef9664539349561eb7360429e5e45515c4602
Randell Dawson <rdawson@onepathtech.com>
2020-01-21 14:35:37 -0800

    fix: converted jquery to vanilla js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(function () {
+function runApp() {
   function getLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(getWeather, getLocationByIP);
@@ -7,10 +7,24 @@ $(function () {
     }
   };
 
+  function checkResponse(response) {
+    if (response.ok) {
+      return response.json();
+    } else {
+      const error = {
+        'statusText': response.statusText,
+        'status': response.status
+      }
+      throw error;
+    }
+  }
+
   function getLocationByIP() {
     const baseUrl = 'https://api.ipgeolocation.io/ipgeo?apiKey=';
     const apiKey = '10d6a9d7232c4e4187c783281e3f82d2';
-    $.ajax(baseUrl + apiKey, { success: getWeather });
+    fetch(baseUrl + apiKey)
+      .then(checkResponse)
+      .then(getWeather);
   };
 
   function getWeather(data) {
@@ -19,31 +33,47 @@ $(function () {
     }
     const baseUrl = "https://apis.randydawson.com/weather/current?";
     const geoCoordsQS = 'lat=' + data.latitude + '&lon=' + data.longitude;
-    $.ajax(baseUrl + geoCoordsQS, { success: updateWeather });
+    fetch(baseUrl + geoCoordsQS)
+      .then(checkResponse)
+      .then(updateWeather);
   };
 
-  function updateWeather(data) {
-    const country = data.sys.country;
-    $("#title").html(data.name);
-    $("#weather-description").html(data.weather[0].main);
-    $("#icon").attr({
-      src: data.weather[0].icon,
-      alt: data.weather[0].description
-    });
-    $("#temp-value").attr("data-celcius", data.main.temp);
+  function updateWeather({
+      sys: { country },
+      name,
+      weather,
+      main: { temp }
+    }) {
+    elems.title.innerHTML = name;
+    const [{ main: weatherDescription, icon, description } ] = weather;
+    elems.weatherDesc.innerHTML =  weatherDescription;
+    elems.icon.setAttribute('src', icon);
+    elems.icon.setAttribute('alt', description);
+    elems.tempValue.setAttribute('data-celcius', temp);
     toggleTemp(country === "US" ? "C" : "F");
-    $("#app").show();
+    elems.app.classList.add('show');
   };
 
   function toggleTemp(unit) {
-    const celcius = +$("#temp-value").attr("data-celcius");
-    unit = $("#degree-unit").html().slice(-1) || unit;
-    $("#temp-value").text(
+    const celcius = +elems.tempValue.getAttribute('data-celcius');
+    unit = elems.degreeUnit.innerHTML.slice(-1) || unit;
+    elems.tempValue.innerText = (
       unit === "C" ? Math.round(celcius * 9 / 5 + 32) : celcius.toFixed(1)
     );
-    $("#degree-unit").html("&#176;" + (unit === "C" ? "F" : "C"));
+    elems.degreeUnit.innerHTML = "&#176;" + (unit === "C" ? "F" : "C");
+  };
+  
+  const elems = {
+    app: document.querySelector('.app'),
+    degreeUnit: document.getElementById('degree-unit'),
+    title: document.getElementById('title'),
+    weatherDesc: document.getElementById('weather-description'),
+    icon: document.getElementById('icon'),
+    tempValue: document.getElementById('temp-value')
   };
 
   getLocation();
-  $("#degree-unit").on("click", toggleTemp);
-});
+  elems.degreeUnit.addEventListener('click', toggleTemp);
+}
+
+document.addEventListener("DOMContentLoaded", runApp);
